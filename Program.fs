6ab3fe39a59d1dc891926ee4f34faad606f5426d
mg0x7BE <108034506+mg0x7BE@users.noreply.github.com>
2026-05-17 00:26:28 +0200

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,60 @@
+﻿open System
+open System.Runtime.InteropServices
+open System.Threading
+
+[<Struct>]
+type POINT =
+    struct
+        val mutable X: int
+        val mutable Y: int
+    end
+
+[<DllImport("user32.dll")>]
+extern bool GetCursorPos([<Out>] POINT& lpPoint)
+
+[<DllImport("user32.dll")>]
+extern bool SetCursorPos(int x, int y)
+
+[<DllImport("user32.dll")>]
+extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo)
+
+let KEYEVENTF_KEYDOWN = 0u
+let KEYEVENTF_KEYUP = 2u
+let VK_F13 = 0x7Cu
+
+let pressKey () =
+    keybd_event (byte VK_F13, 0uy, KEYEVENTF_KEYDOWN, 0u)
+    Thread.Sleep(100) // Small delay before releasing the key
+    keybd_event (byte VK_F13, 0uy, KEYEVENTF_KEYUP, 0u)
+
+let random = Random()
+
+let moveMouse () =
+    let mutable currentPos = POINT()
+
+    if GetCursorPos(&currentPos) then
+        let originalX = currentPos.X
+        let originalY = currentPos.Y
+
+        let direction = random.Next(4)
+
+        let newX, newY =
+            match direction with
+            | 0 -> originalX + 2, originalY
+            | 1 -> (max 0 (originalX - 2)), originalY
+            | 2 -> originalX, originalY + 2
+            | _ -> originalX, (max 0 (originalY - 2))
+
+        SetCursorPos(newX, newY) |> ignore
+        pressKey () // Simulate F13 key press
+
+let jiggleMouse () =
+    while true do
+        moveMouse ()
+        Thread.Sleep(30000)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Program is running. Press Ctrl+C to stop."
+    jiggleMouse ()
+    0
---
+++ README.md
@@ -0,0 +1,29 @@
+
+![CI](https://github.com/mg0x7BE/mouse-jiggler-console/actions/workflows/ci.yml/badge.svg)
+![GitHub repo size](https://img.shields.io/github/repo-size/mg0x7BE/mouse-jiggler-console)
+![GitHub License](https://img.shields.io/github/license/mg0x7BE/mouse-jiggler-console)
+![GitHub Created At](https://img.shields.io/github/created-at/mg0x7BE/mouse-jiggler-console)
+![GitHub forks](https://img.shields.io/github/forks/mg0x7BE/mouse-jiggler-console)
+![GitHub Repo stars](https://img.shields.io/github/stars/mg0x7BE/mouse-jiggler-console)\
+![Windows](https://img.shields.io/badge/Windows-0078D6?logo=windows&logoColor=white)
+![Self-contained](https://img.shields.io/badge/.NET-Self--contained-1F0A32?style=flat&labelColor=8A2BE2)
+
+# Mouse Jiggler Console
+
+Simulates mouse movement to prevent screen lock.
+
+![mouse-jiggler-console.png](gfx/mouse-jiggler-console.png)
+
+## Usage
+
+Run executable. No configuration needed.
+
+Download: [Releases](https://github.com/mg0x7BE/mouse-jiggler-console/releases)
+
+## Note
+
+This is detectable by monitoring software. Check company policy before use.
+
+## License
+
+[Unlicense](LICENSE)
